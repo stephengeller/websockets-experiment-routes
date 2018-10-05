9adbc419bd370127ccb0c34fda65c16d2b06c6c0
Stephen Geller <stephen.geller@hotmail.com>
2018-10-05 17:23:32 +0100

    feat(routes-experiment): Set up simple `/rooms` route for getting name.

--- routes/rooms.js
+++ routes/rooms.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res) {
-    res.send("rooms");
+    res.render("rooms/index");
 });
 
 module.exports = router;
