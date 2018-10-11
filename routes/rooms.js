97c40a2f540ec20fef240008fc4991a231c49fd3
Stephen Geller <stephen.geller@hotmail.com>
2018-10-11 12:56:26 +0100

    feat(routes-experiment): send form to `/rooms/main` and validate input.

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const  { check, validationResult } = require('express-validator/check');
 
 router.get("/", function(req, res) {
     res.render("rooms/index");
 });
 
-router.post("/join", function(req, res) {
-    console.log(req.body);
-    res.send("rooms/join");
+router.post("/main", [
+    check('userName').isLength({ min: 4 })
+] , (req, res) => {
+    const errors = validationResult(req);
+    const { userName } = req.body;
+    console.log(req.body, userName);
+
+    if (errors.isEmpty()) {
+        res.render("rooms/main", {
+            userName
+        });
+    } else {
+        res.send("error");
+    }
+
+
 });
 
 module.exports = router;
